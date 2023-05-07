92dab00a75e5638ab2547d198b9326c265f7b26b
Roman_Kotovski <roman_kotovski@mail.ru>
2023-05-07 17:20:05 +0300

    finish first

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import requests
+import operator
 
 def superhero():
     url = "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json"
     response = requests.get(url=url)
     all_hero = response.json()
-    hero_name = ['Hulk', 'Captain America', 'Thanos', 'Iron Man', 'Thor']
+    hero_name = ['Hulk', 'Captain America', 'Thanos']
     intellegence_hero = {}
     for hero in all_hero:
         if hero['name'] in hero_name:
             intellegence_hero[hero['name']] = hero['powerstats']['intelligence']
-    print(intellegence_hero.items())
+    print(max(intellegence_hero.items(), key=operator.itemgetter(1)))
 
 if __name__ == '__main__':
     superhero()
